4ee19181ba93a0b2bee9e91249e64beb73c3fb69
Caio Moreno <cvinicius.moreno@outlook.com>
2021-09-01 14:37:47 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/caiomoreno/cursomc/CursomcApplication.java
+++ src/main/java/com/caiomoreno/cursomc/CursomcApplication.java
@@ -1,20 +1,27 @@
 package com.caiomoreno.cursomc;
 
 import com.caiomoreno.cursomc.domain.Categoria;
+import com.caiomoreno.cursomc.domain.Produto;
 import com.caiomoreno.cursomc.repositories.CategoriaRepository;
+import com.caiomoreno.cursomc.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(CursomcApplication.class, args);
@@ -25,6 +32,19 @@ public class CursomcApplication implements CommandLineRunner {
         Categoria cat1 = new Categoria(null, "Informática");
         Categoria cat2 = new Categoria(null, "Escritório");
 
-        categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+        Produto p1 = new Produto(null, "Computador", 2000.0);
+        Produto p2 = new Produto(null, "impressora", 800.0);
+        Produto p3 = new Produto(null, "mouse", 80.00);
+
+
+        cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+        cat2.getProdutos().addAll(Arrays.asList(p2));
+
+        p1.getCategorias().addAll(Arrays.asList(cat1));
+        p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+        p3.getCategorias().addAll(Arrays.asList(cat1));
+
+        categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+        produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
     }
 }
---
+++ src/main/java/com/caiomoreno/cursomc/domain/Produto.java
@@ -0,0 +1,56 @@
+package com.caiomoreno.cursomc.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Produto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nome;
+    private Double preco;
+
+    @ManyToMany
+    @JoinTable(
+            name = "PRODUTO_CATEGORIA",
+            joinColumns = @JoinColumn(name = "produto_id"),
+            inverseJoinColumns = @JoinColumn(name = "categoria_id")
+    )
+    private List<Categoria> categorias = new ArrayList<>();
+
+    public List<Categoria> getCategorias() {
+        return categorias;
+    }
+
+    public void setCategorias(List<Categoria> categorias) {
+        this.categorias = categorias;
+    }
+
+    public Produto() {
+    }
+
+    public Produto(Integer id, String nome, Double preco) {
+        this.id = id;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Produto)) return false;
+        Produto produto = (Produto) o;
+        return Objects.equals(id, produto.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
