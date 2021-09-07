179b37ecd2795b5fc3af8510af793b639ff85b36
Caio Moreno <cvinicius.moreno@outlook.com>
2021-09-07 15:52:46 -0300

    Pedido, EstadoPagamento implementação

---
+++ src/main/java/com/caiomoreno/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.caiomoreno.cursomc.repositories;
+
+import com.caiomoreno.cursomc.domain.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
