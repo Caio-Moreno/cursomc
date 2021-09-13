88cff057ff5e965cb583886f7d150978eae970ee
Caio Moreno <cvinicius.moreno@outlook.com>
2021-09-13 16:48:22 -0300

    Correcoes update em categoria service

--- src/main/java/com/caiomoreno/cursomc/services/CategoriaService.java
+++ src/main/java/com/caiomoreno/cursomc/services/CategoriaService.java
@@ -33,8 +33,11 @@ public class CategoriaService {
     }
 
     public Categoria update(Categoria obj) {
-        find(obj.getId());
-        return repository.save(obj);
+        Categoria newObj = find(obj.getId());
+
+        updateData(newObj, obj);
+
+        return repository.save(newObj);
     }
 
     public void delete(Integer id) {
@@ -58,5 +61,9 @@ public class CategoriaService {
 
     public Categoria fromDTO(CategoriaDTO categoriaDTO){
         return new Categoria(categoriaDTO.getId(),categoriaDTO.getNome());
+    }
+
+    private void updateData(Categoria newObj, Categoria obj){
+        newObj.setNome(obj.getNome());
     }
 }
